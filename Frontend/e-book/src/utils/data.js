fca39a095053fcbdfc46ba787db9382f51b53c2f
Milan Gauswami <gauswamimilan17@gmail.com>
2025-12-25 18:49:37 +0530

    finel done

--- Frontend/e-book/src/utils/data.js
+++ Frontend/e-book/src/utils/data.js
@@ -3,6 +3,6 @@
 // export const ORDER_API_ENDPOINT = "http://localhost:3000/api/order";
 
 
-export const USER_API_ENDPOINT = "http://localhost:3000/api/user";
-export const BOOK_API_ENDPOINT = "http://localhost:3000/api/books";
-export const ORDER_API_ENDPOINT = "http://localhost:3000/api/order";
+export const USER_API_ENDPOINT = "https://e-book-18uu.onrender.com/api/user";
+export const BOOK_API_ENDPOINT = "https://e-book-18uu.onrender.com/api/books";
+export const ORDER_API_ENDPOINT = "https://e-book-18uu.onrender.com/api/order";
