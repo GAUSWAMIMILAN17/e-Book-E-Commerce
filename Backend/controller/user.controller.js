f8e66ca495b6ead46bd3fb3db5de4b9b6d5d0c9b
Milan Gauswami <gauswamimilan17@gmail.com>
2025-12-25 18:57:52 +0530

    finel test

--- Backend/controller/user.controller.js
+++ Backend/controller/user.controller.js
@@ -110,7 +110,8 @@ export const login = async (req, res) => {
       .cookie("token", token, {
         maxAge: 24 * 60 * 60 * 1000,
         httpOnly: true,
-        sameSite: "Strict",
+        secure: true,
+        sameSite: "none",
         path: "/",
       })
       .json({
