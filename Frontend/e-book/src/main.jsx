24ce1d500210ff258884d67cc2beb01a1c9e942b
Milan Gauswami <gauswamimilan17@gmail.com>
2025-12-25 18:44:49 +0530

    done

--- Frontend/e-book/src/main.jsx
+++ Frontend/e-book/src/main.jsx
@@ -3,9 +3,9 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
-import { Toaster } from "./Components/ui/sonner.js";
+import { Toaster } from "./components/ui/sonner.js";
 import { Provider } from "react-redux";
-import store, { persistor } from "./Components/redux/store.js";
+import store, { persistor } from "./components/redux/store.js";
 import { PersistGate } from "redux-persist/integration/react";
 
 createRoot(document.getElementById("root")).render(
