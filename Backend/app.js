c68a04d3b4c465fab07c1ab65a9c027f27e2b934
Milan Gauswami <gauswamimilan17@gmail.com>
2026-01-31 18:06:09 +0530

    done Mobile user

--- Backend/app.js
+++ Backend/app.js
@@ -22,8 +22,8 @@ app.use(express.urlencoded({ extended: true }));  //Agar data form se aata hai (
 
 
 const corsOptions = {
-  origin: ["http://localhost:5173"],
-  // origin: ["https://e-book-c1nu.onrender.com"],
+  // origin: ["http://localhost:5173"],
+  origin: ["https://e-book-c1nu.onrender.com"],
   credentials: true,
 };
 
