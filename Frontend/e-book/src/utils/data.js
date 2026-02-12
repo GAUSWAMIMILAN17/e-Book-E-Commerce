a508eb6e5597a38566e2113c02e112e2bda85397
Milan Gauswami <gauswamimilan17@gmail.com>
2026-02-12 10:30:49 +0530

    new

--- Frontend/e-book/src/utils/data.js
+++ Frontend/e-book/src/utils/data.js
@@ -1,8 +1,8 @@
-// export const USER_API_ENDPOINT = "http://localhost:3000/api/user";
-// export const BOOK_API_ENDPOINT = "http://localhost:3000/api/books";
-// export const ORDER_API_ENDPOINT = "http://localhost:3000/api/order";
+export const USER_API_ENDPOINT = "http://localhost:3000/api/user";
+export const BOOK_API_ENDPOINT = "http://localhost:3000/api/books";
+export const ORDER_API_ENDPOINT = "http://localhost:3000/api/order";
 
 
-export const USER_API_ENDPOINT = "https://e-book-18uu.onrender.com/api/user";
-export const BOOK_API_ENDPOINT = "https://e-book-18uu.onrender.com/api/books";
-export const ORDER_API_ENDPOINT = "https://e-book-18uu.onrender.com/api/order";
+// export const USER_API_ENDPOINT = "https://e-book-18uu.onrender.com/api/user";
+// export const BOOK_API_ENDPOINT = "https://e-book-18uu.onrender.com/api/books";
+// export const ORDER_API_ENDPOINT = "https://e-book-18uu.onrender.com/api/order";
